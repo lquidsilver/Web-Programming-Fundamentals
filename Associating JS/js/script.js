97c09caf6d98dd5897fbe3a633291170187e8a80
lquidsilver <lquidsilver@gmail.com>
2013-08-13 12:35:43 -0400

    Added first javascript file

---
+++ Associating JS/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Mike
+ * Date: 8/13/13
+ * Time: 12:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+alert("Hello World");
