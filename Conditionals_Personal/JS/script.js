459f6a0886f9885938207cf67b24cbd72aff5b47
lquidsilver <lquidsilver@gmail.com>
2013-08-21 07:38:58 -0400

    Added first if statement

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -9,5 +9,20 @@
 
 function begin()
 {
-    alert("testing!");
+   var payment = prompt("How are we paying today? Please enter, \"Cash\", \"Credit\", \"Debit\", or \"Check\" ");
+    if(payment == "Cash")
+    {
+        var cashopt1 = prompt("The total is $7.98, what is the exact amount you give?\n **Please do not include the $ symbol**");
+            if(cashopt1 > 7.98)
+            {
+                var dif = 7.98 - cashopt1;
+                alert ("You did not give enough, please give at least $7.98");
+            }
+            else if(cashopt1 == 7.98)
+            {
+                alert ("Thank you for your payment!");
+            }
+
+
+    }
 }
