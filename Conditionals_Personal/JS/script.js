d43a2ac1e302427a6611ad4af5c5bdb6145d6638
lquidsilver <lquidsilver@gmail.com>
2013-08-20 22:27:46 -0400

    created the javascript file for the project

---
+++ Conditionals_Personal/JS/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Arif Michael Wright
+ * Date: 8/20/13
+ * Time: 10:08 PM
+ * Assignment: Conditionals_Personal
+ * To change this template use File | Settings | File Templates.
+ */
+
