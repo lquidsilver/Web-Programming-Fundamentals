d69148e660b93b12e79d4a0832a8ab910f0dd56b
lquidsilver <lquidsilver@gmail.com>
2013-08-21 08:58:15 -0400

    Added more in depth elses to the debit card statements

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -24,29 +24,23 @@ function begin()
             }
             else
             {
-                var dif1 = cashopt1 - 7.98
+                var dif1 = cashopt1 - 7.98;
                 alert ("You gave " +cashopt1+ ", your change will be " +dif1+ ". \n Thank you!");
             }
     }
     else if(payment =="Credit")
     {
         var cardopt1 = prompt("The total is $7.98, are you sure you want to use your credit card? \n Please choose, \"Yes\" or \"No\"");
-        if(cardopt1 == "Yes")
+        if(cardopt1 == "Yes" || "yes")
         {
             prompt("Thank you, please enter your name as an electronic signature");
         }
-        else if(cardopt1 == "yes")
-        {
-            prompt("Thank you, please enter your name as an electronic signature");
-        }
-        else if(cardopt1 == "No")
+
+        else if(cardopt1 == "No" || "no")
         {
             alert("You chose not to use your credit card, please start over");
         }
-        else if(cardopt1 == "no")
-        {
-            alert("You chose not to use your credit card, please start over");
-        }
+
         else
         {
             alert("You did not choose \"Yes\" or \"No\", please start over");
@@ -57,6 +51,20 @@ function begin()
         var debitopt1 = prompt("The total is $7.98, are you sure you want to use your debit card? \n Please choose, \"Yes\" or \"No\"");
         if(debitopt1 == "Yes")
         {
+            var debitopt2 = prompt("Thank you, please enter your 4 digit pin");
+            (debitopt2 < 1000 && debitopt2 >10000) ?  alert("You did not enter a valid pin, please start over") : var debitopt3 = prompt("Thank you, would you like cash back? \n Please choose \"Yes\" or \"No\"");
+            if(debitopt3 == "Yes" || "yes")
+            {
+                var debitopt4 = prompt("How much cash back would you like? \n **Do not add a $ sign**");
+                if(debitopt4 == 0)
+                {
+                    alert("No amount was entered so no cash back is given");
+                }
+                else
+                {
+                    alert("You requested $ " +debitopt4+ ", Thank you!");
+                }
+            }
 
         }
     }
