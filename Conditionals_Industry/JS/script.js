b33b0d9ffb3926110e580cd30eb0a6cefe3c17ff
lquidsilver <lquidsilver@gmail.com>
2013-08-21 21:55:30 -0400

    getting more in depth with the ifs and elses

--- Conditionals_Industry/JS/script.js
+++ Conditionals_Industry/JS/script.js
@@ -9,23 +9,53 @@
 function begin()
 {
     var sfdopt1 = prompt("Which seafood items would you like? Please type \"Fish\", \"Crab\", or \"Shrimp\" ");
+    var fish1 = 7;
+    var crab1 = 8;
+    var shrimp1 = 5;
+    var result
     if(sfdopt1 == "Fish" || "fish")
     {
-        var fish1 = 7;
+
+        var moreopt1 = prompt("Would you like anything else? Please type \"Fish\", \"Crab\",\"Shrimp\", or \"No\"");
+        if(moreopt1 == "Fish" || "fish")
+        {
+            var fish2 = fish1;
+            var moreopt2 = prompt("Would you like anything else? Please type \"Fish\", \"Crab\", \"Shrimp\", or \"No\" \n **Please not, there is a maximum of 3 orders at a time!");
+            if(moreopt2 == "Fish" || "fish")
+            {
+                result = fish2;
+                alert("Your order will take approximately "+result+" minutes, thank you!");
+            }
+            else if(moreopt2 == "Crab" || "crab")
+            {
+                result = fish2 + crab1;
+                alert("Your order will take approximately "+result+" minutes, thank you!");
+            }
+            else if(moreopt2 == "Shrimp" || "shrimp")
+            {
+                result = fish2 + shrimp1;
+                alert("Your order will take approximately "+result+" minutes, thank you!");
+            }
+        }
+        else if(moreopt1 == "Crab" || "crab")
+        {
+
+        }
     }
     else if(sfdopt1 == "Crab" || "crab")
     {
-        var crab1 = 8;
+
+        var moreopt1 = prompt("Would you like anything else? Please type \"Fish\", \"Crab\",\"Shrimp\", or \"No\"");
     }
     else if (sfdopt1 == "Shrimp" ||"shrimp")
     {
-        var shrimp1 = 5;
+
+        var moreopt1 = prompt("Would you like anything else? Please type \"Fish\", \"Crab\",\"Shrimp\", or \"No\"");
     }
     else
     {
         alert("Sorry, please choose a valid option.");
-
     }
-    var moreopt1 = prompt("Would you like anything else? Please type \"Fish\", \"Crab\",\"Shrimp\", or \"No\"");
-    var moreopt2 = prompt("Would you like anything else? Please type \"Fish\", \"Crab\", \"Shrimp\", or \"No\" \n **Please not, there is a maximum of 3 orders at a time!");
+
+
 }
