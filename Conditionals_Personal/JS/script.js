ee9bb29644df6c9e929708d01f6e714897a348f1
lquidsilver <lquidsilver@gmail.com>
2013-08-21 20:04:27 -0400

    finished the script

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -46,10 +46,10 @@ function begin()
             alert("You did not choose \"Yes\" or \"No\", please start over");
         }
     }
-    else if(payment =="Debit")
+    else if(payment == "Debit")
     {
         var debitopt1 = prompt("The total is $7.98, are you sure you want to use your debit card? \n Please choose, \"Yes\" or \"No\"");
-        if(debitopt1 == "Yes")
+        if(debitopt1 == "Yes" || "yes")
         {
             var debitopt2 = prompt("Thank you, please enter your 4 digit pin");
             (debitopt2 < 1000 && debitopt2 >10000) ?  alert("You did not enter a valid pin, please start over") : var debitopt3 = prompt("Thank you, would you like cash back? \n Please choose \"Yes\" or \"No\"");
@@ -58,14 +58,45 @@ function begin()
                 var debitopt4 = prompt("How much cash back would you like? \n **Do not add a $ sign**");
                 if(debitopt4 == 0)
                 {
-                    alert("No amount was entered so no cash back is given");
+                    alert("You selected \"No\", so no cash back is given");
                 }
                 else
                 {
-                    alert("You requested $ " +debitopt4+ ", Thank you!");
+                    alert("You requested $ " +debitopt4+ ", thank you!");
                 }
             }
-
+            else if(debitopt3 == "No" || "no")
+            {
+                alert("Payment received and no cash back was requested, thank you!");
+            }
+            else
+            {
+                alert("You did not choose a valid option, so no cash back will be given");
+            }
         }
+        else if(debitopt1 == "No" || "no")
+        {
+            alert("You chose not to use your debit card, please start over");
+        }
+        else
+        {
+            alert("You did not choose \"Yes\" or \"No\", please start over");
+        }
+    }
+    else if(payment == "Check")
+    {
+        var checkopt1 = prompt("After filling out the check please sign it by entering your name here.");
+        if(checkopt1 == "")
+        {
+            alert("You did not enter a name, please start over");
+        }
+        else
+        {
+            alert("Your payment was received, thank you!");
+        }
+    }
+    else
+    {
+        alert("You did not type a valid payment option, please start over.");
     }
 }
