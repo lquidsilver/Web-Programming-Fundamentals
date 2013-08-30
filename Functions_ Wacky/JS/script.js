329a738774d5f9d0b1f3f49095eabcb35dbb29a6
lquidsilver <lquidsilver@gmail.com>
2013-08-29 23:48:51 -0400

    wrote script

--- Functions_ Wacky/JS/script.js
+++ Functions_ Wacky/JS/script.js
@@ -5,3 +5,10 @@
  * Time: 11:44 PM
  * To change this template use File | Settings | File Templates.
  */
+function begin()
+{
+    var shoes = document.getElementById("shoes").value;
+    var tunic = document.getElementById("tunic").value;
+    var helmet = document.getElementById("helmet").value;
+    document.getElementById("result").innerHTML ="You chose to wear the " +shoes+ " with your " +tunic+ " and " +helmet+ ". Looking good, have fun at your celebration Hero!";
+}
