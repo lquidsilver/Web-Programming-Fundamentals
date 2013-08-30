c5660299609f89110efd8d1bb29da14a8394b753
lquidsilver <lquidsilver@gmail.com>
2013-08-29 21:29:36 -0400

    started script

---
+++ Functions_Personal/JS/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Mike
+ * Date: 8/27/13
+ * Time: 9:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+  function begin()
+{
+    document.getElementById("weight");
+    document.getElementById("activity");
+
+}
