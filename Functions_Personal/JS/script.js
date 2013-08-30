3a00c91cf438fc543f195d4b718c1fe559301e5c
lquidsilver <lquidsilver@gmail.com>
2013-08-29 22:49:13 -0400

    finished the script

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -7,9 +7,17 @@
  */
 
 
-  function begin()
+function begin()
 {
-    document.getElementById("weight");
-    document.getElementById("activity");
+    var weight = document.getElementById("weight").value;
+    var activity = document.getElementById("activity").value;
+    var ounceweight = weight * 0.67;
+    var ounceactivity = activity * 0.4;
+    var total = ounceweight + ounceactivity;
+    var result = Math.ceil(total * 10) / 10;
+    document.getElementById("result").innerHTML = "The suggested amount of water you should consume is " +result+ " ounces of water a day.";
 
 }
+
+
+
