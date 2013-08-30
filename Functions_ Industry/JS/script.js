177ff4763470f3aa9b4c74c9be652055d7c0b385
lquidsilver <lquidsilver@gmail.com>
2013-08-29 23:05:59 -0400

    added some parameters

--- Functions_ Industry/JS/script.js
+++ Functions_ Industry/JS/script.js
@@ -5,3 +5,13 @@
  * Time: 10:51 PM
  * To change this template use File | Settings | File Templates.
  */
+
+function begin()
+{
+    var butter = document.getElementById("butter");
+    var wrap = document.getElementById("wrap");
+    var season = document.getElementById("season");
+
+
+
+}
